f368b9bdf6c7044ed1292255e59a36c67b2420e4
gallo-json <gallojsantiago@gmail.com>
2021-01-10 18:02:37 -0600

    ok finally got it to work;

--- coreutils/link.c
+++ coreutils/link.c
@@ -12,22 +12,26 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 // This is more of an excercise to parse command line arguments
 
 int main(int argc, char *argv[]) {
-    char *original_file; char *linked_file;
+    char original_file[strlen(argv[2])]; char linked_file[strlen(argv[3])];
+
+    strcpy(original_file, argv[2]); strcpy(linked_file, argv[3]);
 
     // program_name = argv[0]
 
     if (argc == 4 && argv[1][0] == '-') {
-        original_file = &argv[2]; linked_file = &argv[3];
         char hard_or_soft = argv[1][1];
 
+        printf("%s %s", original_file, linked_file);
+        
         if (hard_or_soft == 'h') {
             int hardlink = link(original_file, linked_file);
-
+            
             if (hardlink == -1) {
                 fprintf(stderr, "Error: Could not create a hard link.\n");
                 exit(8);
